0aac973da02fecc865e1b64a2105fbbcf8a4d574
Hinesh Miyani <hineshmiyani@gmail.com>
2022-10-24 13:48:54 +0530

    minor fix

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,7 @@
 import { utils } from "ethers";
 import { Contract } from "@ethersproject/contracts";
 import multiSigFactoryAbi from "./MultiSigFactoryAbi.json";
-import contractAddresses from "./contractAddresses.json";
+import contractAddresses from "./ContractAddresses.json";
 
 const contractAddressList: any = contractAddresses;
 export const multiSigFactoryInterface = new utils.Interface(multiSigFactoryAbi);
